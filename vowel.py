c69445534c4280997c12642f87901d552b4bca42
someshwar <someshravi08@gmail.com>
2018-08-09 16:01:13 +0530

    adding 32 files

--- vowel.py
+++ vowel.py
@@ -8,14 +8,30 @@
 # Copyright:   (c) Somesh 2018
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-a = raw_input("Enter a letter")
-b = a.lower()
-first = b[0]
+from Tkinter import*
+from Tkinter import ttk
+from Tkinter import font
+import time
+import datetime
 
-if len(a)>0 and a.isalpha():
-    if first in 'aeiou':
-        print "vowel"
-    else:
-        print "consonant"
-else:
-    print "invalid"
+def quit (*args):
+   root.destroy()
+
+def clock_time():
+   time=datetime.datetime.now()
+   time=(time.strftime("%H:%M:%S"))
+   txt.set(time)
+   root.after(1000,clock_time)
+
+root = Tk()
+root.attributes("=fullscreen",false)
+root.configure(background='black')
+root.bind("x",quit)
+root.after(1000,clock_time)
+
+fnt = font.Font(family='Helvetica',size=120,weight='bold')
+txt = StringVar()
+lbl = ttk.Label(root,textvariable=txt,font=fnt,foreground="white",background="black")
+lbl.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+root.mainloop()
